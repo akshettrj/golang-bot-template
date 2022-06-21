bce6a164dd006b1021d05b6d42a198abfea196f0
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2022-06-21 17:40:27 +0530

    add some middlewares

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module golang-bot-template
 go 1.18
 
 require (
+	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d
 	gopkg.in/telebot.v3 v3.0.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 	gorm.io/driver/mysql v1.3.3
--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -1,7 +1,41 @@
 package middlewares
 
 import (
-    "golang-bot-template/state"
+	"golang-bot-template/state"
 
-    tele "gopkg.in/telebot.v3"
+	"golang.org/x/exp/slices"
+	tele "gopkg.in/telebot.v3"
 )
+
+func OwnerMiddleware(next tele.HandlerFunc) tele.HandlerFunc {
+	ownerID := state.State.Config.Telegram.OwnerID
+	return func(c tele.Context) error {
+		sender := c.Sender()
+		if sender == nil || ownerID != sender.ID {
+			return nil
+		}
+		return next(c)
+	}
+}
+
+func SudoChatMiddleware(next tele.HandlerFunc) tele.HandlerFunc {
+	sudoChats := state.State.Config.Telegram.SudoUsers
+	return func(c tele.Context) error {
+		sender := c.Sender()
+		if sender == nil || !slices.Contains(sudoChats, sender.ID) {
+			return nil
+		}
+		return next(c)
+	}
+}
+
+func AuthorizedChatMiddleware(next tele.HandlerFunc) tele.HandlerFunc {
+	authChats := state.State.Config.Telegram.AuthorizedChats
+	return func(c tele.Context) error {
+		chat := c.Chat()
+		if chat == nil || !slices.Contains(authChats, chat.ID) {
+			return nil
+		}
+		return next(c)
+	}
+}
